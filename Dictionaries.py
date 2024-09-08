efc7ef1082cee38c7961308f203922ad29632f7b
Lalitha Pullela <23P31A4434@acet.ac.in>
2024-09-08 21:23:50 +0530

    d = {}

---
+++ Dictionaries.py
@@ -0,0 +1,5 @@
+d = {} # empty dictionary
+d["sachin"] = 100 # inserts a new key-value pair "sachin": 100
+d["kohli"] = 110 # python will not hold duplicate keys
+d["kohli"] = 80 # key is updated with current value
+print(d)
