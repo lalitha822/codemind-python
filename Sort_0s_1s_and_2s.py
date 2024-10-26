02f27764e07d2212e57d40f0071124ee6ff2f3d4
Lalitha Pullela <23P31A4434@acet.ac.in>
2024-10-26 16:44:08 +0530

    class Solution:

---
+++ Sort_0s_1s_and_2s.py
@@ -0,0 +1,8 @@
+class Solution:
+    # Function to sort an array of 0s, 1s, and 2s
+    def sort012(self, arr):
+        # code here
+        lst = list()
+        a = lst.append(arr.sort())
+        return a
+
