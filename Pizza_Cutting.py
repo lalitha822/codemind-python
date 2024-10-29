8e314c3ec81494d2bad0a2e349662591795f7888
Lalitha Pullela <23P31A4434@acet.ac.in>
2024-10-29 11:00:31 +0530

    t = int(input())

---
+++ Pizza_Cutting.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t = int(input())
+for i in range(t):
+    n = int(input())
+    if(n==1):
+        print("YES")
+    elif(n%2==0):
+        print("YES")
+    else:
+        print("NO")
